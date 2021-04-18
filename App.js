f484d38eb84bd5f622e93b0ff21d803493a4bdf2
muno1 <mounirtaouafe@gmail.com>
2021-04-18 16:02:44 +0200

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {Component} from 'react';
+import { Button, Text, View,StyleSheet,PermissionsAndroid,StatusBar} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Tuner from "./tuner";
+import Note from "./note";
+import Meter from "./meter";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+function SettingsScreen({ navigation }, ciao) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+    
+    <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+const Tab = createBottomTabNavigator();
+class TunerApp {
+  state = {
+    note: {
+      name: "A",
+      octave: 4,
+      frequency: 440,
+    },
+  };
+
+  _update(note) {
+    this.setState({ note });
+  }
+
+  async componentDidMount() {
+    if (Platform.OS === "android") {
+      await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+    }
+
+    const tuner = new Tuner();
+    tuner.start();
+    tuner.onNoteDetected = (note) => {
+      if (this._lastNoteName === note.name) {
+        this._update(note);
+      } else {
+        this._lastNoteName = note.name;
+      }
+    };
+  }
+  
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator>
+    
+    <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  </NavigationContainer>
+  );
+}
+const style = StyleSheet.create({
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
-});
+  frequency: {
+    fontSize: 28,
+    color: "#37474f",
+  },
+})
+
+;
